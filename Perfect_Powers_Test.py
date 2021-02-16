e99751e457f8c9f5141958f15fd6deccf3e21937
alexvu <moonmtwo@gmail.com>
2021-02-16 15:55:12 +0700

    modify PerfectPower to use fast powering

--- Perfect_Powers_Test.py
+++ Perfect_Powers_Test.py
@@ -1,3 +1,17 @@
+def truncated_fast_powering(g,a, N):
+    u = a
+    s = g % N
+    c = 1
+    while u >= 1:
+        if u % 2 != 0:
+            c = (c*s)
+            if(c > N):
+                c = N + 1
+                break
+        s = (s*s) % N
+        u = u//2
+    return c
+
 def perfect_power(n):
     b = 2
     if n < 2:
@@ -7,7 +21,7 @@ def perfect_power(n):
         c = n
         while c - a >= 2:
             m = (a+c)//2
-            p = min(m**b, n+1)
+            p = truncated_fast_powering(m,b,n)
             if p == n:
                 return True
             if p < n:
